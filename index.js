bef80b293399d8c786a6544844eb3978d799adcf
Michael Dimitras <michaeladimitras@gmail.com>
2017-09-08 19:46:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"];
 
 
-function destructivelyAppendKitten(arr, name){
+function destructivelyAppendKitten(name){
   kittens.push(name);
   return kittens;
 }
 
-function destructivelyPrependKitten(arr, name)
+function destructivelyPrependKitten(arr, name){
+  kittens.unshift(name);
+  retuen kittens;
+}
